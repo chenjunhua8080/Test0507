741c577022c144d51dc59aa3ffbb244edc3bf097
chenjunhua <chenjunhua@qingzu.com.cn>
2019-09-21 16:01:50 +0800

    改项目名

--- src/main/java/com/example/demo/MpToolsApplication.java
+++ src/main/java/com/example/demo/MpToolsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeChatMpToolsApplication {
+public class MpToolsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeChatMpToolsApplication.class, args);
+		SpringApplication.run(MpToolsApplication.class, args);
 	}
 }
